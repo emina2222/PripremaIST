35a4a3f663f983a2f6f199adad191b356459eae2
Emina <emarmarac22@gmail.com>
2020-04-28 21:48:20 +0200

    Obrisan fajl klijent.js

--- zadatak5/server.js
+++ zadatak5/server.js
@@ -1,1 +1,19 @@
 const http=require('http')
+const url=require('url')
+const fs=require('fs')
+const querystring=require('query-string')
+
+const server=http.createServer(function(request,response){
+    let urlObj=url.parse(request.url,true,false)
+    if(request.method=='POST'){
+
+    }
+    else if(request.method=='GET'){
+
+    }
+});
+
+const port=2000;
+const host='127.0.0.1';;
+server.listen(port,host)
+console.log(`Server radi na adresi: ${host}: ${port}`);
